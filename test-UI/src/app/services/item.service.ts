a637c3f52f063d83a1e68d3dbcffeae1ebba4645
Maxim Bureiko <mlmaxgm@gmail.com>
2018-04-05 15:29:18 +0300

    rdy Item

--- test-UI/src/app/services/item.service.ts
+++ test-UI/src/app/services/item.service.ts
@@ -4,31 +4,41 @@ import {Item} from "../classes/item";
 @Injectable()
 export class ItemService{
 
-  items: Item[];
   nextId: number;
 
   constructor() {
-    this.items = [
-      new Item(0, "First Item"),
-      new Item(1, "Second Item"),
-      new Item(2, "Third Item")
-    ];
+    let items = this.getItems();
 
-    this.nextId = 3;
+    if (items.length == 0) {
+      this.nextId = 0;
+    } else {
+      let maxId = items[items.length - 1].id;
+      this.nextId = maxId + 1;
+    }
   }
 
   addItem(text: string){
     let item = new Item(this.nextId, text);
-    this.items.push(item);
+    let items = this.getItems();
+    items.push(item);
+
+    this.setLocalStorageItems(items);
     this.nextId++;
   }
 
   getItems(): Item[] {
-    return this.items;
+    let locatStorageItem = JSON.parse(localStorage.getItem('items'));
+    return locatStorageItem == null ? [] : locatStorageItem.items;
   }
 
   deleteItem(id: number){
-    this.items = this.items.filter((item)=> item.id !== id);
+    let items = this.getItems();
+    items = items.filter((item)=> item.id != id);
+    this.setLocalStorageItems(items);
+  }
+
+  setLocalStorageItems(items: Item[]) {
+    localStorage.setItem('items', JSON.stringify({items: items}));
   }
 
 }
